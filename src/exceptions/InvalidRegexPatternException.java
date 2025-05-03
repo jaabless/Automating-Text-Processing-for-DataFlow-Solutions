d74302bfd372c4a14e4104c3c2e7f00060efd5ad
jaabless <jefferyasamoahbless@gmail.com>
2025-05-03 23:45:28 +0000

    InvalidRegexPatternException for invalid regex patterns

--- src/exceptions/InvalidRegexPatternException.java
+++ src/exceptions/InvalidRegexPatternException.java
@@ -1,9 +1,6 @@
 package exceptions;
 
 public class InvalidRegexPatternException extends RuntimeException {
-    public InvalidRegexPatternException(String message) {
-        super(message);
-    }
 
     public InvalidRegexPatternException(String messages, Throwable cause) {
         super(messages, cause);
