f9d663c73f24f3b1057e6ec06bc43ccbc7aa6aef
jaabless <jefferyasamoahbless@gmail.com>
2025-05-04 00:01:46 +0000

    Create Main entry point to launch the application

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        ui.TextProcessorApp.launch(ui.TextProcessorApp.class);
+        ui.TextProcessorApp.launch(ui.TextProcessorApp.class);//to launch the application
     }
 }
